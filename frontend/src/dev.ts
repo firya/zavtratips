bb76abdf9a8c306aaf91019b552952412678f7d3
firya <firya@bk.ru>
2022-07-11 13:59:39 +0300

    fix dockerfiles

--- frontend/src/dev.ts
+++ frontend/src/dev.ts
@@ -8,4 +8,4 @@ const ngrockUrl = "https://8478-5-253-204-100.eu.ngrok.io";
 export const hostURL =
 	process.env.NODE_ENV == "development"
 		? `${ngrockUrl}`
-		: `https://${process.env.VIRTUAL_HOST}:3000`;
+		: `https://${process.env.VIRTUAL_HOST}`;
