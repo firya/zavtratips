1c9c2986ce5ff058be2e76c2456aa663e3d5c750
lebedevm <lebedevm@ozon.ru>
2023-08-18 18:20:21 +0300

    fix routing

--- backend/router.ts
+++ backend/router.ts
@@ -4,16 +4,12 @@ import { telegramBotInit } from "./bot";
 export const router = (app: Express) => {
   const { bot, secretPath } = telegramBotInit();
 
-  app.use("/webapp", express.static("../frontend/dist"));
-  app.use("/webapp/*", express.static("../frontend/dist"));
+  app.get("/webapp", express.static("../frontend/dist"));
+  app.get("/webapp/*", express.static("../frontend/dist"));
 
   app.use(bot.webhookCallback(secretPath));
 
   app.get("/", (req: Request, res: Response) => {
     res.send("Get out! You are not welcome here");
-  });
-
-  app.get("/test", (req: Request, res: Response) => {
-    res.send("Get out!");
   });
 };
