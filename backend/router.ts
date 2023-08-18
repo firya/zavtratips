4143e548db46f6c5e8ec2ed9fa33ae8985465d53
lebedevm <lebedevm@ozon.ru>
2023-08-18 17:27:27 +0300

    test vercel deployment 6

--- backend/router.ts
+++ backend/router.ts
@@ -2,12 +2,12 @@ import express, { Express, Request, Response } from "express";
 import { telegramBotInit } from "./bot";
 
 export const router = (app: Express) => {
-  // const { bot, secretPath } = telegramBotInit();
+  const { bot, secretPath } = telegramBotInit();
 
-  // app.use("/webapp", express.static("../frontend/dist"));
-  // app.use("/webapp/*", express.static("../frontend/dist"));
+  app.use("/webapp", express.static("../frontend/dist"));
+  app.use("/webapp/*", express.static("../frontend/dist"));
 
-  // app.use(bot.webhookCallback(secretPath));
+  app.use(bot.webhookCallback(secretPath));
 
   app.get("/", (req: Request, res: Response) => {
     res.send("Get out! You are not welcome here");
