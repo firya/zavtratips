9ac3c180ff114571087f46d74ed896baf29c1677
lebedevm <lebedevm@ozon.ru>
2023-08-18 17:45:11 +0300

    test remove dotenv

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,14 @@
 import express, { Express } from "express";
+import dotenv from "dotenv";
 import { router } from "./router";
 import { DB, createAllTables } from "./db";
 import { cronJobs } from "./cron";
+
+if (process.env.NODE_ENV === "dev") {
+  dotenv.config({
+    path: "../.env",
+  });
+}
 
 const app: Express = express();
 const port = process.env.PORT || "8080";
