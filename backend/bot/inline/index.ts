7fb693d9dc6e1957c546d558a3d136b7d92980f2
lebedevm <lebedevm@ozon.ru>
2023-11-17 15:16:16 +0300

    reset inline answer cache every 30 seconds

--- backend/bot/inline/index.ts
+++ backend/bot/inline/index.ts
@@ -4,23 +4,28 @@ import { getRecommendationInlineResults } from "./getRecommendations";
 
 export const inline = Composer.on("inline_query", async (ctx) => {
   const { query } = ctx.inlineQuery;
-  console.log(query);
+
   const results = [
     ...(await getStatInlineResults(query)),
     ...(await getRecommendationInlineResults(query)),
   ];
 
   return await ctx.answerInlineQuery(
-    results.slice(0, 50).map((item, i) => ({
-      id: i.toString(),
-      type: "article",
-      thumb_url: item.image,
-      title: item.title,
-      description: item.description,
-      input_message_content: {
-        message_text: item.message,
-        parse_mode: "Markdown",
+    results.slice(0, 50).map(
+      (item, i) => ({
+        id: i.toString(),
+        type: "article",
+        thumb_url: item.image,
+        title: item.title,
+        description: item.description,
+        input_message_content: {
+          message_text: item.message,
+          parse_mode: "Markdown",
+        },
+      }),
+      {
+        cache_time: 30,
       },
-    })),
+    ),
   );
 });
